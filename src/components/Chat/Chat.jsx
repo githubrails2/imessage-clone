28d26a5a0bc1bfdb7380006efc11b945f2dd6570
Wilburn E White <soniclinuxknight@hotmail.com>
2021-01-30 05:09:22 -0600

    Added rules to Chat.css and made chat__messages flex: 1

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -3,7 +3,19 @@ import "./Chat.css";
 const Chat = () => {
   return (
     <div className="chat">
-      <h1>I am Chat</h1>
+      <div className="chat__header">
+        <h4>
+          TO: <span className="chat__name">ChannelName</span>
+        </h4>
+        <strong>Details</strong>
+      </div>
+      <div className="chat__messages">WE are the messages</div>
+      <div className="chat__input">
+        <form>
+          <input type="text" placeholder="Enter a Message" />
+          <button>Send Message</button>
+        </form>
+      </div>
     </div>
   );
 };
