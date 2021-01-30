d1478f85930112d83925adea02cb51865e9b94a5
Wilburn E White <soniclinuxknight@hotmail.com>
2021-01-30 03:52:32 -0600

    Added SidebarChat and CSS

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,24 @@
+import { Avatar, IconButton } from "@material-ui/core";
+import { RateReviewOutlined, Search } from "@material-ui/icons";
 import React from "react";
 import "./Sidebar.css";
+import SidebarChat from "./SidebarChat/SidebarChat";
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <h1>I am Sidebar</h1>
+      <div className="sidebar__header">
+        <Avatar src="" className="sidebar__avatar" />
+        <div className="sidebar__input">
+          <Search />
+          <input type="text" placeholder="Search" />
+        </div>
+        <IconButton variant="outlined" className="sidebar_inputButton">
+          <RateReviewOutlined />
+        </IconButton>
+      </div>
+      <div className="sidebar__chats">
+        <SidebarChat />
+      </div>
     </div>
   );
 };
